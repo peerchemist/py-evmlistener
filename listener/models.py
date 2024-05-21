c16cab7d01511294af28a95bc089f87f3471ddda
peerchemist <dx.1178@gmail.com>
2024-05-21 12:09:16 +0200

    add __str__ method to BurnEvent dataclass

--- listener/models.py
+++ listener/models.py
@@ -9,6 +9,12 @@ class BurnEvent:
     block_number: int
     txid: str
 
+    def __str__(self) -> str:
+        return (
+            f"BurnEvent(who={self.who}, amount={self.amount}, unwrap_address={self.unwrap_address}, "
+            f"block_number={self.block_number}, txid={self.txid})"
+        )
+
 
 @dataclass
 class MintEvent:
